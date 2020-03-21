bebf5651eb0e847f3aefaa077c7a6ffc7fd024c2
Jonah Henriksson <33059163+JonahHenriksson@users.noreply.github.com>
2020-03-21 14:19:01 -0400

    Created Pong Game

---
+++ src/main.rs
@@ -0,0 +1,59 @@
+
+use amethyst::{
+    audio::{AudioBundle, DjSystemDesc},
+    core::TransformBundle,
+    prelude::*,
+    renderer::{
+        plugins::{RenderFlat2D, RenderToWindow},
+        types::DefaultBackend,
+        RenderingBundle
+    },
+    utils::application_root_dir,
+    input::{
+        InputBundle,
+        StringBindings
+    },
+    ui::{RenderUi, UiBundle}
+};
+
+mod pong;
+use crate::pong::Pong;
+
+mod systems;
+
+mod audio;
+use crate::audio::Music;
+
+fn main() -> amethyst::Result<()> {
+    amethyst::start_logger(Default::default());
+
+    let app_root = application_root_dir()?;
+    let display_config_path = app_root.join("config").join("display.ron");
+
+    let binding_path = app_root.join("config").join("bindings.ron");
+    let input_bundle = InputBundle::<StringBindings>::new().with_bindings_from_file(binding_path)?;
+
+    let game_data = GameDataBuilder::default()
+        .with_bundle(TransformBundle::new())?
+        .with_bundle(input_bundle)?
+        .with_bundle(UiBundle::<StringBindings>::new())?
+        .with_bundle(AudioBundle::default())?
+        .with_system_desc(DjSystemDesc::new(|music: &mut Music| music.music.next()), "dj_system", &[])
+        .with(systems::PaddleSystem, "paddle_system", &["input_system"])
+        .with(systems::MoveBallsSystem, "ball_system", &[])
+        .with(systems::BounceSystem, "collision_system", &["paddle_system", "ball_system"])
+        .with(systems::WinnerSystem, "winner_system", &["ball_system"])
+        .with_bundle(RenderingBundle::<DefaultBackend>::new()
+            .with_plugin(RenderToWindow::from_config_path(display_config_path)?
+                .with_clear([0.0, 0.0, 0.0, 1.0]))
+            .with_plugin(RenderFlat2D::default())
+            .with_plugin(RenderUi::default()))?;
+
+    let assets_dir = app_root.join("assets");
+    let mut world = World::new();
+    let mut game = Application::new(assets_dir, Pong::default(), game_data)?;
+
+    game.run();
+
+    Ok(())
+}
